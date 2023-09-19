36c639cf1c8f41c440ed0ede8be6c7cc62b4858b
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2023-09-20 01:57:12 +0930

    Add files via upload

---
+++ python programs class3/module2.py
@@ -0,0 +1,56 @@
+# import module1
+# print(module1.x)
+
+# import keyword
+# print("there are total", len(keyword.kwlist), "keywords in python")
+# print(keyword.kwlist)
+
+
+# a = 5**6400
+# print(a) 
+
+
+
+#operators
+# a = 4*5
+# print(a) 
+
+# b = 4-3
+# print(b)
+
+# c = 10/5
+# print(c)
+
+# d = 3//4
+# print(d)
+
+# e = 11%5
+# print(d)
+
+# f = 11>5  
+# g = 45<4
+# h = not 3>4
+# print(f,g,h)
+
+#identity operator
+x=5
+y=5
+
+print(id(x))
+print(id(y))
+
+
+a = x is y 
+print(a)
+
+
+b = "my phone  is poco m2 pro"
+print("my" not in b)
+
+# print("Enter your name")
+x = input("Enter your name")
+print("Hello", x)
+
+
+
+
