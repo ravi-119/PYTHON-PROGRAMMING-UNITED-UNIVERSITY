d6df32dcbc4913e11fbff7ea163b5f278a48d804
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2023-09-20 01:56:35 +0930

    Add files via upload

---
+++ Class2.py
@@ -0,0 +1,56 @@
+# print("hello world")    # printing message on the screen 
+
+
+# """
+# author ravi kumar yadav 
+# developed in 2023
+
+# """
+
+
+
+
+# this is a program to add two numbers 
+print("Enter two numbers")  # taking input from users
+a = int(input())
+b = int(input())
+c = a+b
+print("sum is", c)
+
+
+# 
+print("Enter two numbers")
+a = int(input())
+b = int(input())
+ans = a*b
+print(" multiplccation of two numer is ", ans)
+
+
+# x = 5.55
+# y = 5
+# z = "Ravi"
+# a = 3j
+
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+# print(type(a))
+
+# a = 5
+# b = 7 
+# c = 6
+# print(a,b,c)
+
+#to print the value of a, b, c
+# a = 5
+# b = 6
+# c = 7
+# print(a,b,c)  #ans 5 6 7 
+# print(a,b,c, sep="-")
+# print(a,b,c, sep=" ")
+# print(a,b,c, end="$")
+# print("Hello")
+
+# print("Hello")
+# print("Ravi Kumar Yadav")
